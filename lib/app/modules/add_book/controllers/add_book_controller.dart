24bf1857ed5368cf7af902c4a91d4fe179770d37
JorryBiksu <jorryc01@email.com>
2024-01-17 14:07:44 +0700

    add pinjam dan buku

---
+++ lib/app/modules/add_book/controllers/add_book_controller.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:try_pee/app/data/constant/endpoint.dart';
+import 'package:try_pee/app/data/provider/api_provider.dart';
+import 'package:dio/dio.dart' as dio;
+
+class AddBookController extends GetxController {
+  //TODO: Implement AddBookController
+  final GlobalKey<FormState> formKey= GlobalKey<FormState>();
+  final TextEditingController judulController = TextEditingController();
+  final TextEditingController penulisController = TextEditingController();
+  final TextEditingController penerbitController = TextEditingController();
+  final TextEditingController tahunController = TextEditingController();
+
+  final count = 0.obs;
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+
+  void increment() => count.value++;
+
+  final loadingBook = false.obs;
+  addBook()async {
+    loadingBook(true);
+    try{
+      FocusScope.of(Get.context!).unfocus();
+      formKey.currentState?.save();
+      if (formKey.currentState!.validate()) {
+        final response = await ApiProvider.instance().post(Endpoint.book,
+            data:
+              {
+                "judul": judulController.text.toString(),
+                "penulis": penulisController.text.toString(),
+                "penerbit": penerbitController.text.toString(),
+                "tahun_terbit": int.parse(tahunController.text.toString())
+              }
+            );
+        if (response.statusCode == 201) {
+          Get.back();
+        } else {
+          Get.snackbar("Sorry", "Login Gagal", backgroundColor: Colors.orange);
+        }
+      }
+      loadingBook(false);
+    } on dio.DioException catch (e) {
+      loadingBook(false);
+      Get.snackbar("Sorry", e.message ?? "", backgroundColor: Colors.red);
+    } catch (e) {
+      loadingBook(false);
+      Get.snackbar("Error", e.toString(), backgroundColor: Colors.red);
+      throw Exception('Error: $e');
+    }
+  }
+}
